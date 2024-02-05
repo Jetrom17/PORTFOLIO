d38c4737ba971c51bcc46d5453982c7b186abf3c
jeiel <github.esvlc@slmail.me>
2024-02-05 11:30:52 -0300

    update

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
 console.log('%cFLOOD, SPAM ou SCAM será ignorado!', 'color: red; font-size: 16px; font-weight: bold;');
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Esperar 3 segundos antes de exibir o alerta
-    setTimeout(function() {
-      var siteAlert = document.getElementById('site-alert');
-  
-      // Verificar se o alerta já foi fechado
-      if (localStorage.getItem('siteAlertClosed') !== 'true') {
-        // Adicionar a classe 'show' para exibir o alerta
-        siteAlert.classList.add('show');
-      }
-    }, 5000);
-  });
-  
-  function closeAlert() {
-    var siteAlert = document.getElementById('site-alert');
-    
-    // Esconder o alerta
-    siteAlert.style.display = 'none';
-  
-    // Armazenar o fechamento do alerta no armazenamento local
-    localStorage.setItem('siteAlertClosed', 'true');
-  }
-
   // Discord - SPAM ou Flood ignorado
 
   function enviarFormulario() {
